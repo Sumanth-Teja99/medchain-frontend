d1b5b67098c9b9f83da574eaad314da9911c8526
Nani-theblocked <sumanthkvs1995@gmail.com>
2026-03-27 12:49:59 +0530

    update backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://medchain-blockchain-backend.onrender.com",
+  baseURL: " https://medchain-blockchain-backend.onrender.com",
 });
 
 export default API;
